9cd90223f49e89538a578b3ad3834f195e0d73f5
tk914 <yeluoyh@outlook.com>
2022-09-05 17:00:30 +0800

    删除baseUrl

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import "font-awesome/css/font-awesome.min.css";
 import "animate.css"
 import "comtoolcss"
 
-// axios.defaults.baseURL = "http://127.0.0.1:15672";
 Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false;
